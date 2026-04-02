9590400621d99ba8c7f21c291250c9947318cd3f
winnieepm <71612467+winnieepm@users.noreply.github.com>
2026-04-02 15:42:23 -0400

    reverse blog order, change date structure

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,6 +5,14 @@ module.exports = async function(eleventyConfig) {
     eleventyConfig.setTemplateFormats(["njk", "md", "html"]);
     eleventyConfig.addPassthroughCopy("css/styles.css");
     eleventyConfig.addFilter("limit", (array, n) => (Array.isArray(array) ? array : []).slice(0, n));
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+      return dateObj.toLocaleString(undefined, {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+    });
+
 
     return {
     dir: {
